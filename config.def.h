e6debcb33579dc2a82543061cee05b2d5a868b1f
kilpo <none@mail.com>
2022-02-25 03:18:43 +0200

    applied bar padding patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Ubuntu Mono:size=12" };
 static const char dmenufont[]       = "Ubuntu Mono:size=12";
 static const char col_gray1[]       = "#222222";
