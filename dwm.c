4a407bd0340972e9fdb45a5881749484e3b80c83
kilpo <none@mail.com>
2022-02-26 21:55:14 +0200

    add padding for status text in bar

--- dwm.c
+++ dwm.c
@@ -730,8 +730,8 @@ drawbar(Monitor *m)
 	/* draw status first so it can be overdrawn by tags later */
 	if (m == selmon) { /* status is only drawn on selected monitor */
 		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad + 2; /* 2px right padding */
-		drw_text(drw, m->ww - tw - 2 * sp, 0, tw, bh, 0, stext, 0);
+		tw = TEXTW(stext) - lrpad + 12; /* 2px right padding */
+		drw_text(drw, m->ww - tw - 2 * sp, 0, tw, bh, 6, stext, 0);
 	}
 
 	for (c = m->clients; c; c = c->next) {
